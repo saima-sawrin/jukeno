47d374c4611acc9e2ac72720484fbc229e498961
Saima <Saima@DESKTOP-VDQAE47>
2024-10-03 17:21:33 +0600

    Fix responsive for home and about page

--- js/swiper.js
+++ js/swiper.js
@@ -16,7 +16,7 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   var swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 10,
+    slidesPerView: 8,  // Default for large screens
     spaceBetween: 30,
     autoplay: {
       delay: 2500,
@@ -30,4 +30,25 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+    breakpoints: {
+      // when window width is >= 320px (phones)
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      },
+      // when window width is >= 768px (tablets)
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      },
+      // when window width is >= 1024px (desktops)
+      1024: {
+        slidesPerView: 8,
+        spaceBetween: 30
+      },
+      1536: {
+        slidesPerView: 10,
+        spaceBetween: 30
+      }
+    }
+});
